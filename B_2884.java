3d2dc73fb3c65ab670ddc5f3f81c349f9179774c
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-04-15 23:40:45 +0900

    Add files via upload

---
+++ B_2884.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str =  br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (m<45){
+            h--;
+            m = 60 - (45-m);
+            if (h<0){
+                h=23;
+            }
+            System.out.println(h + " "+ m);
+        }else {
+            System.out.println(h + " "+ (m-45));
+        }
+
+
+
+
+    }
+}
