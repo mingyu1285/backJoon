cf5d4f55d76fe9fc44c20778daa564ce38958cf3
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-05-07 23:14:55 +0900

    Add files via upload

---
+++ B_2743.java
@@ -0,0 +1,12 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class B_2743 {
+    public static void main(String[] args) {
+        Scanner s  = new Scanner(System.in);
+        String text = s.nextLine();
+
+        System.out.println(text.length());
+    }
+}
